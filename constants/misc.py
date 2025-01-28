209ac59489b5864c82785edd55527200e1b9c97e
Aditya-Sakpal <aditya.as@somaiya.edu>
2025-01-28 22:40:36 +0530

    requirementss.txt updated

--- constants/misc.py
+++ constants/misc.py
@@ -3,7 +3,8 @@ from models.calculation import Run, CoverageUnitsRec,CashFlow,Liability_Init_Rec
 
 # DB_URL="postgresql://infigos_solutions_user:U640zVayfFvOTg4S6U7hN14dy2UoVHBS@dpg-ctiudvdumphs73f70t90-a.singapore-postgres.render.com/infigos_solutions"
 
-DB_URL = "postgresql://postgres:postgres@localhost:5432/infigos_db_2"
+DB_URL = "postgresql://user:SLyfZfXToOaFPO7w6RhH0EIftTdl9ZjU@dpg-cucgmkrv2p9s73d7k9s0-a.singapore-postgres.render.com/infigos_db_2"
+# DB_URL = "postgresql://postgres:postgres@localhost:5432/infigos_db_2"
 
 TABLE_MODEL_MAPPING = {
     "Run": Run,
