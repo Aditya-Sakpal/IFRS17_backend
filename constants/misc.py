0d8ffa3b7ec72f45f4a50ae2bb6da5d7688b64fe
Aditya Sakpal <aditya.as@somaiya.edu>
2025-04-21 00:03:45 +0530

    Removed secret

--- constants/misc.py
+++ constants/misc.py
@@ -3,8 +3,10 @@ from models.calculation import Run, CoverageUnitsRec,CashFlow,Liability_Init_Rec
 
 # DB_URL="postgresql://infigos_solutions_user:U640zVayfFvOTg4S6U7hN14dy2UoVHBS@dpg-ctiudvdumphs73f70t90-a.singapore-postgres.render.com/infigos_solutions"
 
-DB_URL = "postgresql://user:J6TmlGJ0DWzyUGEucZh4fJ40AT0Iwq7V@dpg-cvcp4mt2ng1s73913fo0-a.singapore-postgres.render.com/infigos_db"
+# DB_URL = "postgresql://user:J6TmlGJ0DWzyUGEucZh4fJ40AT0Iwq7V@dpg-cvcp4mt2ng1s73913fo0-a.singapore-postgres.render.com/infigos_db"
 # DB_URL = "postgresql://postgres:postgres@localhost:5432/infigos_db_2"
+
+DB_URL = "postgres://avnadmin:AVNS_WqF8fc_ApocbwKYmR9n@infigosdb-ifrs.j.aivencloud.com:16431/defaultdb?sslmode=require"
 
 TABLE_MODEL_MAPPING = {
     "Run": Run,
