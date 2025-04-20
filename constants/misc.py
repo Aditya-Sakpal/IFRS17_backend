4fa03729f17ee2051bd51eb494fdd1cb1a378dca
Aditya Sakpal <aditya.as@somaiya.edu>
2025-04-21 02:17:25 +0530

    changed db url

--- constants/misc.py
+++ constants/misc.py
@@ -6,7 +6,7 @@ from models.calculation import Run, CoverageUnitsRec,CashFlow,Liability_Init_Rec
 # DB_URL = "postgresql://user:J6TmlGJ0DWzyUGEucZh4fJ40AT0Iwq7V@dpg-cvcp4mt2ng1s73913fo0-a.singapore-postgres.render.com/infigos_db"
 # DB_URL = "postgresql://postgres:postgres@localhost:5432/infigos_db_2"
 
-DB_URL = "postgres://avnadmin:AVNS_WqF8fc_ApocbwKYmR9n@infigosdb-ifrs.j.aivencloud.com:16431/defaultdb?sslmode=require"
+DB_URL = "postgresql://avnadmin:AVNS_WqF8fc_ApocbwKYmR9n@infigosdb-ifrs.j.aivencloud.com:16431/defaultdb?sslmode=require"
 
 TABLE_MODEL_MAPPING = {
     "Run": Run,
